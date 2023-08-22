38d0f7b9cae7a09158a36568c3b56627e39ff0f1
Bahar Ulus <baharuluss1@gmail.com>
2023-08-22 22:36:41 +0300

    variables not alındı

---
+++ user_basic.dart
@@ -0,0 +1,22 @@
+void main(List<String> args) {
+  
+
+// yeni bir user adı oluştur
+// yeni bir para ver
+// bu parayı 20 ye böl ve ekranda göster.
+
+double userMoney = 30;
+String userName = 'Bahar';
+userMoney = userMoney / 20;
+// userMoney += 5; bunu bu şekilde yap
+
+print('Merhaba $userName, $userMoney tl ye sahipsin');
+
+print('------------------');
+userMoney -= 10;
+print('Paranız değer kaybetti $userMoney');
+// double -> küsüratlar da kullanılan değişken türü
+
+
+}
+
---
+++ week1/start.dart
@@ -0,0 +1,13 @@
+void main() {
+  // bu main fonksiyonu bir dart projesinin başlangıcı oluyor.
+  print("Hello my first project");
+  /*
+  variable
+  -verileri saklamayı ve yönetmeyi sağlıyor.
+  -veri adı anlamlı olmalı
+  */ 
+  String verininAdi = "verinin degeri";
+  String hello = "Hello3";
+  
+
+} 
